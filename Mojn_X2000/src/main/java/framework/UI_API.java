5d4241fc8ed24ad604c16deb3c36352e663aac95
Andreas Engly <s170303@student.dtu.dk>
2019-04-07 16:59:36 +0200

    API updated

--- Mojn_X2000/src/main/java/framework/UI_API.java
+++ Mojn_X2000/src/main/java/framework/UI_API.java
@@ -9,7 +9,11 @@ public class UI_API {
 	public String RegisterPatient(String firstName, String lastName, String adress, String tribe, int day, int month, int year, boolean alive) {
 		
 		try {
+			
 			Patient patient = new Patient(firstName, lastName, adress, tribe, day, month, year, alive);
+			
+			
+			
 			return String.format("%s %s registered succesfully!", firstName, lastName);
 			
 		} catch (IllegalArgumentException e) {
