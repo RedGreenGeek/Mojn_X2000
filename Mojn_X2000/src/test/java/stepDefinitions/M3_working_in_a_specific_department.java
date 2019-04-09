20209c7fda578d3f80c07536328e9aa7b45f59cf
s <s@s.s>
2019-04-09 20:36:52 +0200

    Til Andreas

---
+++ Mojn_X2000/src/test/java/stepDefinitions/M3_working_in_a_specific_department.java
@@ -0,0 +1,44 @@
+package stepDefinitions;
+
+public class M3_working_in_a_specific_department {
+	
+	
+	
+	@Given("^with departments$")
+	public void with_departments() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^I want to see who is working in a specific department$")
+	public void i_want_to_see_who_is_working_in_a_specific_department() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I am entering a correct department name\\.$")
+	public void i_am_entering_a_correct_department_name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I get a list of staff member for the specific department\\.$")
+	public void i_get_a_list_of_staff_member_for_the_specific_department() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I am entering a incorrect department name\\.$")
+	public void i_am_entering_a_incorrect_department_name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I get a message that the department is not found$")
+	public void i_get_a_message_that_the_department_is_not_found() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
