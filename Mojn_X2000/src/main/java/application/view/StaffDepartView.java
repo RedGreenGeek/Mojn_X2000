e09aac4b1a9a05952bea5ebecdeba235b894ab4f
Anton Ruby Larsen <anton96121@gmail.com>
2019-05-02 09:36:09 +0200

    Refactor of gui grid layout on master from wednesday afternoon

--- Mojn_X2000/src/main/java/application/view/StaffDepartView.java
+++ Mojn_X2000/src/main/java/application/view/StaffDepartView.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import application.controller.StaffDepartController;
+import application.utils.GridBagLayoutUtils;
 
 @SuppressWarnings("serial")
 public class StaffDepartView extends JFrame {
@@ -84,58 +85,19 @@ public class StaffDepartView extends JFrame {
 	public void layoutComponents() {
 		setLayout(new GridBagLayout());
 		
-		GridBagConstraints gc = new GridBagConstraints();
-		
-		/////////////////////////////////// 1. linje
-		
+		int y = 0;
+		inputArea.add(departmentNameLabel,  GridBagLayoutUtils.constraint(0, y, 1, 0.1, 0, 0, 0, 5, GridBagConstraints.LINE_END));
 
-		
-		///////////////////////////// 2. linje
-		
-
-		/////////////////////////// 3. linje
-		
-
-		
-		/////////////////////////// 4. linje
-		
-
-		
-		/////////////////////////// 5. linje
-		
-
-		
-		/////////////////////////// 6. linje
-		
-		gc.gridx = 0;
-		gc.gridy++;
-		gc.weightx = 1;
-		gc.weighty = 0.1;
-		gc.fill = GridBagConstraints.NONE;
-		gc.anchor = GridBagConstraints.LINE_END;
-		gc.insets = new Insets(0,0,0,5); // (top, left, bottom, right)
-		
-		inputArea.add(departmentNameLabel, gc);
-		
-		gc.gridx = 1;
-		gc.anchor = GridBagConstraints.LINE_START;
-		gc.insets = new Insets(0,0,0,0);
-		
-		inputArea.add(departmentNameField, gc);
+		inputArea.add(departmentNameField, GridBagLayoutUtils.constraint(1, y, 1, 0.1, 0, 0, 0, 0, GridBagConstraints.LINE_START));
 		
 		
 		/////////////////////////// 7. linje
 		
-		gc.gridx = 1;
-		gc.gridy++;
-		gc.weightx = 1;
-		gc.weighty = 1;
-		gc.anchor = GridBagConstraints.FIRST_LINE_START;
-		gc.insets = new Insets(0,0,0,0);
 		
-		inputArea.add(okBtn, gc);
+		y = y+1;
+		
+		inputArea.add(okBtn, GridBagLayoutUtils.constraint(1, y, 1, 1, 0, 0, 0, 0, GridBagConstraints.FIRST_LINE_START));
 		inputArea.setBorder(new EmptyBorder(10,10,10,10));
-		
 		
 		//////////////////////////// TEXT AREA //////////////////////////////7
 		setLayout(new BorderLayout());
