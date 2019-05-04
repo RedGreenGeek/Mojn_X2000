0c6a0a1ee917a97fe24bd764962d1e1ea7cf5792
s <s@s.s>
2019-05-04 14:57:56 +0200

    Birthday test

---
+++ Mojn_X2000/src/test/java/jUTest/Test_CheckBirthday_Test.java
@@ -0,0 +1,28 @@
+package jUTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import framework.API;
+
+public class Test_CheckBirthday_Test {
+	API api = API.getInstance();
+	 
+	  @Test
+	  public void simpleTest() {
+		  api.registerPatient("I", "I", "I", "I", "I", "I", 29, 2, 1996,false);
+		assertTrue(api.patientSearcher("I", "I", "", "I", "", "", "").contains("I"));
+		assertEquals("Additional information is needed.", api.registerPatient("I", "I", "A", "A", "A", "A", 30, 2, 1996,false));
+		  api.registerPatient("I", "I", "B", "B", "B", "B", 30, 4, 1996,false);
+		assertTrue(api.patientSearcher("I", "I", "", "B", "", "", "").contains("B"));
+		assertEquals("Additional information is needed.", api.registerPatient("I", "I", "C", "C", "C", "C", 32, 4, 1996,false));  
+		  api.registerPatient("I", "I", "D", "D", "D", "D", 31, 3, 1996,false);
+		assertTrue(api.patientSearcher("I", "I", "", "D", "", "", "").contains("D"));
+		assertEquals("Additional information is needed.", api.registerPatient("I", "I", "E", "E", "E", "E", 32, 3, 1996,false)) ;
+		assertEquals("Additional information is needed.", api.registerPatient("I", "I", "F", "F", "F", "F", 31, 3, 1996,false)) ;
+		assertTrue(api.patientSearcher("I", "I", "", "F", "", "", "").contains("F"));
+	    System.err.println("Palle er gud");
+	  }
+
+	}
